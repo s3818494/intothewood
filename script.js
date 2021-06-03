74d24d810bb6ce82744c897285deeec606cbc989
s3818494 <81259532+s3818494@users.noreply.github.com>
2021-06-04 03:29:44 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
 
-    isClicked = false;
+    isClicked = false; // check if isClicked is true or false
 
     $(document).ready(function(){
 
@@ -12,16 +12,13 @@ document.addEventListener("DOMContentLoaded", () => {
         $(window).scroll(function() {
 
             // current scrolltop value/scroll position
-
-            // if the current scrolltop is bigger than the last scrolltop -> scrolling down
             var scroll = $(window).scrollTop(); 
 
-            if (scroll > position) {
+            if (scroll > position) {  // if the current scrollTop is bigger than the last scrollTop -> scrolling down
 
                 $("#inst").fadeOut(1500);
 
                 console.log('scrollDown');
-                // console.log($(window).scrollTop);
 
                 if ((scroll >= 0) && (scroll < 300)) {
                     $("#tree1").fadeTo(0, 0);
@@ -42,6 +39,7 @@ document.addEventListener("DOMContentLoaded", () => {
                     $("#tree3").fadeTo(0, 1);
                     $("#text3").fadeTo(0, 1);
                 }
+                
                 if ((scroll >= 2400) && (scroll < 2700)) {
                     $("#tree3").fadeTo(0, 0);
                     $("#text3").fadeTo(0, 0);
@@ -62,11 +60,12 @@ document.addEventListener("DOMContentLoaded", () => {
                 }
 
                 if ((scroll >= 4800) && (scroll < 5100)) {
-                    $(".black-bg").stop().animate({top: '0vh'}, 1500);
+                    $(".black-bg").stop().animate({top: '0vh'}, 1500); //slide up to top: 0vh position
+                    console.log("go up")
                 }
 
                 
-            } else { // else, the current scrolltop is smaller than the last scrolltop -> scrolling up
+            } else { // else, the current scrollTop is smaller than the last scrollTop -> scrolling up
                 if ((scroll >= 0) && (scroll < 300)) {
                     $("#tree1").fadeTo(0, 1);
                     $("#title1").fadeIn(1500); 
@@ -106,7 +105,7 @@ document.addEventListener("DOMContentLoaded", () => {
                 }
 
                 if ((scroll >= 4800) && (scroll < 5100)) {
-                    $(".black-bg").stop().animate({top: '100vh'}, 1500);
+                    $(".black-bg").stop().animate({top: '100vh'}, 1500); // slide down back to top: 100vh position
                     console.log("go down")
                 }
                 
@@ -115,16 +114,15 @@ document.addEventListener("DOMContentLoaded", () => {
             }
             position = scroll; // save the last scrolltop value/the last scroll position
 
-
         });
 
 
         // ------------- FORCE THE SCROLLBAR TO GO BACK TO THE TOP AFTER REFRESHING -----------------------
 
-        history.scrollRestoration = "manual"; //set the scroll restoration behavior on history navigation to manual to prevent it go back to the old location
+        history.scrollRestoration = "manual"; // set the scroll restoration behavior on history navigation to manual to prevent it go back to the old location
 
-        $(window).on('beforeunload', function(){ //beforeunload event fired before the webpage and its resources are about to be unloaded
-            $(window).scrollTop(0); //back to 0px when webpage is reloaded
+        $(window).on('beforeunload', function(){ // beforeunload event fired before the webpage and its resources are about to be unloaded
+            $(window).scrollTop(0); // back to 0px when webpage is reloaded
         });
 
 
@@ -133,16 +131,14 @@ document.addEventListener("DOMContentLoaded", () => {
 
         $(function() {
 
-            var hour = (new Date).getHours();
-
-            // var hour = 20;
+            var hour = (new Date).getHours(); // get computer hours
         
-            if (hour >= 6 && hour <= 17) {
-            $('.day').css("opacity", 1);
-            $("#day_audio").css("display", "flex");
-            } else {
-            $('.night').css("opacity", 1);
-            $("#night_audio").css("display", "flex");
+            if (hour >= 6 && hour <= 17) { // from 6h to 17h, display day mode
+                $('.day').css("opacity", 1);
+                $("#day_audio").css("display", "flex");
+            } else { // other time, display night mode
+                $('.night').css("opacity", 1);
+                $("#night_audio").css("display", "flex");
             }
             console.log(hour)
 
@@ -153,11 +149,11 @@ document.addEventListener("DOMContentLoaded", () => {
         // -------------- TOGGLE FUNCTION FOR HIGH CONTRAST/WEB ACCESSIBILITY BUTTON -----------------
 
         $("#contrast").click(function() {
-            if ( isClicked === false ) {
-                isClicked = true;
+            if ( isClicked === false ) { // hasnt clicked/click again, execute this block of code -> turn off high contrast mode
+                isClicked = true; // boolean variable that signals when condition exists -> isClick boolean turned on 
 
                 $("#contrast").css("border-color", "white");
-                $("#contrast").css("background-color", "rgba(0, 0, 0, 0.5");
+                $("#contrast").css("background-color", "rgb(0, 0, 0)");
                 $("#contrast").css("color", "white");
 
                 $(".white-bg").css("opacity", 0);
@@ -179,16 +175,11 @@ document.addEventListener("DOMContentLoaded", () => {
                 $("#text5").css("color", "white");
                 $("#text5").css("background-color", "transparent");
 
-                // $("#tree2").css("opacity", 0.2);
-                // $("#tree3").css("opacity", 0.15);
-                // $("#tree4").css("opacity", 0.1);
-                // $("#tree5").css("opacity", 0.05);
-                
                 console.log("clicked again");
               } 
               
-            else {
-                isClicked = false;
+            else { // click, execute this block of code -> turn on high contrast mode
+                isClicked = false; // boolean variable that signals when condition does not exist -> isClicked boolean turned off
 
                 $("#contrast").css("border-color", "black");
                 $("#contrast").css("background-color", "rgb(204, 204, 204)");
@@ -216,10 +207,8 @@ document.addEventListener("DOMContentLoaded", () => {
                 console.log("clicked");
               }
         });
-
     
     });
-
 
 })
 
